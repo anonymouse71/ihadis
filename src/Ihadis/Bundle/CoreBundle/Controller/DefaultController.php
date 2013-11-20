04e8646fc8e9f70a71bb6fab84d80c72adb27500
Emran Hasan <phpfour@gmail.com>
2013-11-20 10:33:09 +0600

    Added front-page layout.

--- src/Ihadis/Bundle/CoreBundle/Controller/DefaultController.php
+++ src/Ihadis/Bundle/CoreBundle/Controller/DefaultController.php
@@ -6,7 +6,7 @@ use Symfony\Bundle\FrameworkBundle\Controller\Controller as BaseController;
 
 class DefaultController extends BaseController
 {
-    public function indexAction($name)
+    public function indexAction()
     {
         return $this->render('IhadisCoreBundle:Default:index.html.twig');
     }
