7df7072ace6f539b6f7189ee2622099cd35c5c70
Emran Hasan <phpfour@gmail.com>
2013-12-07 13:32:18 +0600

    Some small changes.

--- src/Ihadis/Bundle/CoreBundle/Resources/public/js/ihadis.js
+++ src/Ihadis/Bundle/CoreBundle/Resources/public/js/ihadis.js
@@ -1,5 +1,7 @@
 var IHadis = function () {
 
+    var clip;
+
     var handleTranslationToggle = function() {
 
         $(".translation a").live('click', function(){
@@ -14,9 +16,28 @@ var IHadis = function () {
 
     }
 
+
+
+    var initZeroClipboard = function() {
+        ZeroClipboard.setDefaults( { moviePath: '/bundles/ihadiscore/js/ZeroClipboard.swf' } );
+        clip = new ZeroClipboard();
+    }
+
+    var handleHadithCopy = function() {
+        $('.copy-hadith').click(function(){
+            var hadithNum = $(this).data('id');
+            var lang = $(this).parent().parent().find('.translation a.active').data('id');
+            var content = $('#hadith-body-' + hadithNum + '-' + lang).text();
+            console.log(content);
+            clip.setText(content);
+        })
+    }
+
     return {
 
         init: function() {
+            initZeroClipboard();
+            //handleHadithCopy();
             handleTranslationToggle();
         }
 
