dab42337a21d448a5c135718a94d3f2a57637bdd
Anis Ahmad <anisniit@gmail.com>
2014-01-26 17:23:27 +0600

    Some Homepage updates and dtatic pages

---
+++ src/Ihadis/Bundle/WebBundle/Controller/StaticPagesController.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace Ihadis\Bundle\WebBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+
+class StaticPagesController extends Controller
+{
+    public function aboutUsAction()
+    {
+        return $this->render('IhadisWebBundle:StaticPages:aboutUs.html.twig');
+    }
+
+    public function disclaimerAction()
+    {
+        return $this->render('IhadisWebBundle:StaticPages:disclaimer.html.twig');
+    }
+
+    public function joinTeamAction()
+    {
+        return $this->render('IhadisWebBundle:StaticPages:joinTeam.html.twig');
+    }
+
+    public function feedbackAction()
+    {
+        return $this->render('IhadisWebBundle:StaticPages:feedback.html.twig');
+    }
+
+}
