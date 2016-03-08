72c213f52f751f76a73a823985c1e19950c4f99d
Anis Ahmad <anis.programmer@gmail.com>
2016-03-08 16:56:59 +0600

    Disabled Exceptions in prod env

--- web/app.php
+++ web/app.php
@@ -16,8 +16,8 @@ $loader->register(true);
 
 require_once __DIR__.'/../app/AppKernel.php';
 //require_once __DIR__.'/../app/AppCache.php';
-Debug::enable();
-$kernel = new AppKernel('prod', true);
+//Debug::enable();
+$kernel = new AppKernel('prod', false);
 $kernel->loadClassCache();
 //$kernel = new AppCache($kernel);
 $request = Request::createFromGlobals();
